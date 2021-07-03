9fe00ad5c3e1271c8723692514107ea52333be90
jillasaikrishna545 <jillasaikrishna545@gmail.com>
2021-07-03 20:51:46 +0530

    fixed syntax issue

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='It's an wafer project using mlops.',
+    description='Its an wafer project using mlops.',
     author='Jilla Sai krishna',
     license='MIT',
 )
